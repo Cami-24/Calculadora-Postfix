4cda5b4da149b56661d8d2e6512493ffa981dc41
Camila <cdgolondrina@gmail.com>
2026-02-01 13:46:15 -0600

    interfaz

---
+++ IStack.java
@@ -0,0 +1,7 @@
+public interface IStack<T> {
+    void push(T item); //agrega valor a la pila
+    T pop();           //quita y devuelve ultimo valor agregado
+    T peek();          //ve el ultimo valor agregado sin quitarlo
+    boolean isEmpty(); //verifica si la pila esta vacia
+    int size();        //devuelve el numero de elementos en la pila
+}
